7ae1eb2957ccc6e31449915c9c2185a958f0332a
PaulTechCode <168778788+PaulTechCode@users.noreply.github.com>
2024-06-17 14:20:25 -0300

    Update MenuAdmController.java

--- Controller/MenuAdmController.java
+++ Controller/MenuAdmController.java
@@ -1,15 +1,24 @@
-package Controller;
+package com.example.projeto_library;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class MenuAdmController {
+public class MenuAdmController implements Initializable {
 
     @FXML
     StackPane area_trabalho;
@@ -25,6 +34,17 @@ public class MenuAdmController {
 
     @FXML
     private JFXButton botao_update;
+
+    @FXML
+    private Label mensagem;
+
+    private Stage stage;
+
+    private Scene scene;
+
+    private Parent root;
+
+    AdmsDaoImpl admsDao = AdmsDaoImpl.getInstance();
 
 
     public void acervo_atualizar(ActionEvent event) throws IOException
@@ -54,5 +74,20 @@ public class MenuAdmController {
         Parent root = FXMLLoader.load((getClass().getResource("configs_adm.fxml")));
         area_trabalho.getChildren().removeAll();
         area_trabalho.getChildren().setAll(root);
+    }
+
+    public void sair(ActionEvent event)throws  IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("escolha_login.fxml"));
+        root = fxmlLoader.load();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        mensagem.setText("Seja Bem-Vindo ao Nosso App: "+admsDao.getAdm(admsDao.getIndex()).getNome()+"!");
     }
 }
